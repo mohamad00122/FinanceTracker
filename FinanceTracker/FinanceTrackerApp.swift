1fc024740dbc290626b34e25c04708cd0db50a4f
BigDaddy57 <mohamad00122@gmail.com>
2025-04-15 20:58:43 -0700

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Mohamad Chokr on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceTrackerTests/FinanceTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FinanceTrackerTests.swift
+//  FinanceTrackerTests
+//
+//  Created by Mohamad Chokr on 4/15/25.
+//
+
+import Testing
+
+struct FinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
